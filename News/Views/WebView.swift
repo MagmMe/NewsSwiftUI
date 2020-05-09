88ec90138f5d006d19d99f133ee077b08152b390
MagmMe <marcin@magm.me>
2020-05-09 07:21:45 +0200

    Creating navigation between List View nad Detail View

---
+++ News/Views/WebView.swift
@@ -0,0 +1,29 @@
+//
+//  WebView.swift
+//  News
+//
+//  Created by Marcin Magiera on 09/05/2020.
+//  Copyright © 2020 Magme Agency. All rights reserved.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let request: URLRequest
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let WebView = WKWebView(frame: .zero)
+        return WebView
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        uiView.load(request)
+    }
+}
+
+struct WebView_Previews: PreviewProvider {
+    static var previews: some View {
+        WebView(request: URLRequest(url: URL(string:"https://learnappmaking.com/ex/lipsum.html")!))
+    }
+}
