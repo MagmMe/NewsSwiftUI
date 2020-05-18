83e97e5fd24ad59d9130293bfc3a8d27905ed502
MagmMe <marcin@magm.me>
2020-05-18 18:51:46 +0200

    bug fixes

--- News/Views/ArticleList.swift
+++ News/Views/ArticleList.swift
@@ -13,7 +13,7 @@ let articles = [
     Article(title: "KORONAWIRUS – RAPORT DZIENNY – 11 MAJA", author: "Redakcjas", url: "https://learnappmaking.com/ex/lipsum.html"),
     Article(title: "Nakręćmy się dla Nikolki, czyli podaruj nakrętki chorej dziewczynce", author: "TRedakcja", url: "https://learnappmaking.com/ex/lipsum.html"),
     Article(title: "Innowacyjna szkoła z niepowtarzalną atmosferą – FOTO", author: "Redakcja", url: "https://learnappmaking.com/ex/lipsum.html"),
-    Article(title: "World Wide Web Revolutionizes the Internet", author: "USA Today", url: "https://learnappmaking.com/ex/lipsum.html"),
+    Article(title: "World Wide Web Revolutionizes the Internet", author: "Redakcja", url: "https://learnappmaking.com/ex/lipsum.html"),
     Article(title: "Perfumy prosto z wybiegu? Linie kosmetyków wielkich domów mody", author: "Redakcja", url: "https://learnappmaking.com/ex/lipsum.html"),
     Article(title: "800 litrów nakrętek dla Nikolki – FOTO", author: "Redakcja", url: "https://learnappmaking.com/ex/lipsum.html"),
     Article(title: "Podbijają sportowcy, amatorzy, politycy i dyrektorzy szkół – FILMY", author: "Redakcja", url: "https://learnappmaking.com/ex/lipsum.html"),
